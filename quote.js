846019c76404ea6a04736689cd8ef8783ad8be1d
Samarth Ghule <92852347+SamarthGhule@users.noreply.github.com>
2023-07-21 16:58:15 +0530

    added index.html

---
+++ quote.js
@@ -0,0 +1,18 @@
+let quote = document.getElementById("quote");
+let author = document.getElementById("author");
+let btn = document.getElementById("btn");
+
+const url = "https://api.quotable.io/random";
+// const url = "https://api.quotable.io/quotes/random?minLength=100&maxLength=140";
+
+let getQuote = () => {
+    fetch(url)
+    .then((data) => data.json())
+    .then((item) =>{
+        quote.innerText = item.content;
+        author.innerText = item.author;
+    });
+};
+
+window.addEventListener("load", getQuote);
+btn.addEventListener("click",getQuote);
