9f1368ae7495cd63e57f500729ca8e2d98ae9c89
Samarth Ghule <92852347+SamarthGhule@users.noreply.github.com>
2023-08-04 23:22:53 +0530

    Update api quote.js

--- quote.js
+++ quote.js
@@ -2,9 +2,9 @@ let quote = document.getElementById("quote");
 let author = document.getElementById("author");
 let btn = document.getElementById("btn");
 
-// const url = "https://api.quotable.io/random";
-const url = "https://favqs.com/api/";
+// const url = "https://favqs.com/api/";
 // const url = "https://api.quotable.io/quotes/random?minLength=100&maxLength=140";
+const url = "https://api.quotable.io/random";
 
 let getQuote = () => {
     fetch(url)
